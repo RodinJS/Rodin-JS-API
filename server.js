25ffbbb0c291203e8cb63a98bfa2b92bf283071c
Grigør <grigor@xachatryan.com>
2016-09-07 12:08:09 +0400

    Added process.json file and some modules

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
  */
 const express = require('express');
 const logger = require('morgan');
+const bodyParser = require("body-parser");
+const compression = require('compression');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const dotenv = require('dotenv');
 const chalk = require('chalk');
+const cors = require('cors');
+
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
@@ -33,23 +37,47 @@ mongoose.connection.on('error', () => {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 6666);
+app.set('port', process.env.PORT || 7000);
+app.use(compression());
 app.use(logger('dev'));
+app.use(cors());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(function(req, res, next) {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
-      req.path !== '/login' &&
-      req.path !== '/signup' &&
-      !req.path.match(/^\/auth/) &&
-      !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  }
-  next();
+app.use(bodyParser.json({limit: '5mb'}));
+// app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(function(req, res, next) {
+//   // After successful login, redirect back to the intended page
+//   if (!req.user &&
+//       req.path !== '/login' &&
+//       req.path !== '/signup' &&
+//       !req.path.match(/^\/auth/) &&
+//       !req.path.match(/\./)) {
+//     req.session.returnTo = req.path;
+//   }
+//   next();
+// });
+
+
+app.get('/', (req, res) => {
+	res.json({
+		"error": "",
+		"response": "Gago aper"
+	});
 });
+
+
+
+
+
+
+
+
+
+
 
 /**
  * Start Express server.
