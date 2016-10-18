ce6ba95ab5c01f2992cc61f933ae61481f390709
Grigør <g@yvn.io>
2016-10-18 20:42:03 +0400

    helpers refactor

--- server/helpers/editor.js
+++ server/helpers/editor.js
@@ -1,17 +1,3 @@
-function lookupMime(filename) {
-    var ext = /[^\/\\.]*$/.exec(filename)[0];
-    return {
-        js: "application/javascript",
-        ico: "image/x-icon",
-        css: "text/css",
-        svg: "image/svg+xml",
-        png: "image/png",
-        jpg: "image/jpg",
-        html: "text/html",
-        jpeg: "image/jpeg"
-    }[ext];
-}
-
 // don't let users crawl up the folder structure by using a/../../../c/d
 function cleanUrl(url) {
     url = decodeURIComponent(url);
