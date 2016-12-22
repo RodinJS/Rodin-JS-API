25f8125c6495c9dabf8540fe9c13afd369cf1ec3
Grigør <g@yvn.io>
2016-12-22 15:19:34 +0400

    custom domain Part4: fix

--- config/env/development.js
+++ config/env/development.js
@@ -80,5 +80,5 @@ export default {
   },
   mandrill:'ouOYaHWxlDaabLYVjrG1BA',
   stuff_path: '/var/www/stuff/',
-  nginx_template_path: '/var/www/api.rodin.psace/resources/nginx/'
+  nginx_template_path: '/var/www/api.rodin.space/resources/nginx/'
 };
