18080bdc1d9a8c8ebd8adf3f3d43d0944e89d5f4
Grigør <g@yvn.io>
2016-12-22 16:46:05 +0400

    documentation update

--- server/routes/domains.js
+++ server/routes/domains.js
@@ -20,61 +20,44 @@ router.route('/')
 	*          "domain": "sub.example.com",
 	*     }
 	*
-	* @apiSuccess (200) {String} success              Success message <code>true</code>.
+	* @apiSuccess (200) {String} success             Success message <code>true</code>.
 	* @apiSuccess (200) {Object} data                Data object.
-	* @apiSuccess (200) {String} data.name          GitHub repo name.
-	* @apiSuccess (200) {String} data.private          <code>private/public</code>.
-	* @apiSuccess (200) {String} data.git_url        GitHub repo ssh url.
-	* @apiSuccess (200) {String} data.clone_url        GitHub repo https url.
-	* @apiSuccess (200) {String} data.location          GitHub repo url.
-	* @apiSuccess (200) {String} data.status          GitHub repo creation status.
-	* @apiSuccess (200) {Object} data.branch            Branch object.
-	* @apiSuccess (200) {String} data.branch.message        Message from GitHub API.
-	* @apiSuccess (200) {String} data.branch.repo_url    GitHub repo https url.
+	* @apiSuccess (200) {String} data.message        Success message.
 	*
 	* @apiSuccessExample {json} Success-Response:
-	*  HTTP/1.1 200 OK
+	*  HTTP/2 200 OK
 	*  {
 	*	  "success": true,
 	*	  "data": {
-	*	    "name": "vavandr",
-	*	    "private": false,
-	*	    "git_url": "git://github.com/grigorkh/vavandr.git",
-	*	    "clone_url": "https://github.com/grigorkh/vavandr.git",
-	*	    "location": "https://api.github.com/repos/grigorkh/vavandr",
-	*	    "status": "201 Created",
-	*	    "branch": {
-	*	      "message": "rodin_editor branch successfuly created",
-	*	      "repo_url": "https://github.com/grigorkh/vavandr.git"
-	*	    }
+	*	    "message": "t2.rodin.design domain name added to project successfuly!"
 	*	  }
 	*	}
 	*
 	* @apiError NoGithub GitHub account not linked to this user!
 	*
 	* @apiErrorExample {json} Bad Request:
-	*      HTTP/1.1 350
-	*      {
-	*          "success": false
-	*          "error": {
-	*              "message": "GitHub account not linked to this user!",
-	*              "status": 350,
-	*              "timestamp": 1473863313415
-	*          }
-	*      }
+	*      HTTP/2 360
+	*		{
+	*			"success": false,
+	*			"error": {
+	*				"message": "Domain name does not provided!",
+	*				"status": 360,
+	*				"timestamp": 1482410469168
+	*			}
+	*		}
 	*
 	* @apiError NoToken Token does not provided!
 	*
 	* @apiErrorExample {json} Bad Request:
-	*      HTTP/1.1 315
-	*      {
-	*          "success": false
-	*          "error": {
-	*              "message": "Token does not provided!",
-	*              "status": 315,
-	*              "timestamp": 1480923413253
-	*          }
-	*      }
+	*      HTTP/2 328
+	*		{
+	*			"success": false,
+	*			"error": {
+	*				"message": "Project id does not provided!",
+	*				"status": 328,
+	*				"timestamp": 1482410651163
+	*			}
+	*		}
 	*
 	*/
 	.post(check.ifTokenValid, domainsCtrl.add);
