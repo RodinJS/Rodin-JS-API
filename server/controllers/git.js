9c59b86146d74f1e9790d407a3a72b97695c0d8f
xgharibyan <khachatur.gharibyan@gmail.com>
2017-01-13 15:25:26 +0400

    update create git command

--- server/controllers/git.js
+++ server/controllers/git.js
@@ -7,6 +7,7 @@ import git from '../helpers/github';
 import Project from '../models/project';
 import request from 'request-promise';
 import _ from 'lodash';
+import shell from '../helpers/shell';
 
 
 const APIURLS = {
@@ -104,9 +105,61 @@ function create(req, res, next) {
         try{
           process.chdir(projectRoot);
           console.log(`New directory: ${process.cwd()}`);
-          require('simple-git')(projectRoot)
+
+          shell.series([
+            'git init',
+            'git add -A',
+            'git commit -m \"initial commit!\"',
+            `git remote add origin  ${result.data.clone_url}`,
+            `git push - u origin ${result.data.clone_url}`
+          ], projectRoot, (err) => {
+            console.log('git push error: ', err);
+            Project.findOneAndUpdateAsync(
+              {
+                _id: req.body.id,
+                owner: req.user.username
+              },
+              {
+                $set: {
+                  github: {
+                    git: result.data.git_url,
+                    https: result.data.clone_url
+                  }
+                }
+              },
+              {
+                new: true
+              }).then(projData => {
+              let repo_info = result;
+              git.createBranch(req.user.username, help.cleanUrl(req.body.id), projectRoot, "rodin_editor")
+                .then(result => {
+                  return res.status(200).json({
+                    success: true,
+                    data: {
+                      name: repo_info.data.name,
+                      private: repo_info.data.private,
+                      git_url: repo_info.data.git_url,
+                      clone_url: repo_info.data.clone_url,
+                      location: repo_info.data.meta.location,
+                      status: repo_info.data.meta.status,
+                      branch: result
+                    }
+                  });
+                })
+                .catch(e => {
+                  const err = new APIError('Can\'t update info', httpStatus.BAD_REQUEST, true);
+                  return next(e);
+                });
+            }).catch(e => {
+              const err = new APIError('Can\'t update info', httpStatus.BAD_REQUEST, true);
+              return next(e);
+            });
+          });
+
+
+         /* require('simple-git')(projectRoot)
             .init()
-            .add('./*')
+            .add('./!*')
             .commit("initial commit!")
             .addRemote('origin', result.data.clone_url)
             .push(['-u', repo_url], () => {
@@ -150,7 +203,8 @@ function create(req, res, next) {
                 const err = new APIError('Can\'t update info', httpStatus.BAD_REQUEST, true);
                 return next(e);
               });
-            });
+            });*/
+
         }
         catch(e){
           console.log(e);
