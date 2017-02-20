bad9a39fa709399d2c7736d5d4c32eae39fca021
Grigør <g@yvn.io>
2017-02-21 00:00:31 +0400

    RO-233 #updated templates for work with lib's v0.0.2 version

---
+++ resources/templates/simple/systemjs/config.js
@@ -0,0 +1,49 @@
+(function (global) {
+
+	var paths = {
+		'npm:': 'https://cdn.rodin.io/v0.0.2/'
+	};
+
+	var map = {
+		'rodin/core': 'npm:core',
+		'rodin/physics': 'npm:physics'		
+	};	
+
+	var packages = {
+		'dist': { main: 'index.js', defaultExtension: 'js' },
+		'rodin/core': { main: 'index.js', defaultExtension: 'js' },
+		'rodin/physics': { main: 'index.js', defaultExtension: 'js' },
+	};
+
+	var moduleNames = [
+		'core/error',
+        'core/time',
+        'core/scene',
+        'core/sculpt',
+        'core/messenger',
+        'core/eventEmitter',
+        'core/utils',
+        'core/set',
+        'core/initializer',
+        'core/constants',
+        'core/rodinEvent',
+        'core/raycaster',
+        'core/controllers',
+        'core/animation'
+	];
+
+	function packIndex(moduleName) {
+		packages['' + paths['npm:'] + moduleName + ''] = { main: 'index.js', defaultExtension: 'js' };
+	}
+
+	moduleNames.forEach(packIndex);
+
+	var config = {
+		paths: paths,
+		map: map,
+		packages: packages
+	};
+
+	System.config(config);
+
+})(this);
