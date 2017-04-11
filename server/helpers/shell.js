22fdb330d09e9d604324c51ec833ecbd4ada2b0d
Grigør <g@yvn.io>
2017-04-11 17:28:54 +0400

    RO-243 #debugging in dev server

--- server/helpers/shell.js
+++ server/helpers/shell.js
@@ -1,33 +1,40 @@
 // execute a single shell command where "cmd" is a string
 exports.exec = (cmd, dir, cb) => {
-    var child_process = require('child_process');
-    var parts = cmd.split(/\s+/g);
-    var p = child_process.spawn(parts[0], parts.slice(1), { stdio: 'inherit', cwd: dir });
-    p.on('exit', (code) => {
-        var err = null;
-        if (code) {
-            err = new Error(`command '${cmd}' exited with wrong status code '${code}'`);
-            err.code = code;
-            err.cmd = cmd;
-            err.dir = dir;
-        }
+	var child_process = require('child_process');
+	var parts = cmd.split(/\s+/g);
+	var p = child_process.spawn(parts[0], parts.slice(1), { stdio: 'inherit', cwd: dir });
+	p.on('exit', (code) => {
+		var err = null;
+		if (code) {
+			err = new Error(`command '${cmd}' exited with wrong status code '${code}'`);
+			err.code = code;
+			err.cmd = cmd;
+			err.dir = dir;
+		}
 
-        if (cb) cb(err);
-    });
+		if (cb) cb(err);
+	});
+	p.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	p.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`);
+	});
 };
 
 // execute multiple commands in series
 exports.series = (cmds, dir, cb) => {
-    var execNext = () => {
-        exports.exec(cmds.shift(), dir, (err) => {
-            if (err) {
-                cb(err);
-            } else {
-                if (cmds.length) execNext();
-                else cb(null);
-            }
-        });
-    };
+	var execNext = () => {
+		exports.exec(cmds.shift(), dir, (err) => {
+			if (err) {
+				cb(err);
+			} else {
+				if (cmds.length) execNext();
+				else cb(null);
+			}
+		});
+	};
 
-    execNext();
+	execNext();
 };
