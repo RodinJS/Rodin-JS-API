2d03d532e28944eabee3d1650462849b19f8ad60
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-08-11 12:09:14 +0400

    RO-1332 # get pages grouped by category

--- server/models/pages.js
+++ server/models/pages.js
@@ -2,6 +2,7 @@ import Promise from 'bluebird';
 import mongoose from 'mongoose';
 import httpStatus from '../helpers/httpStatus';
 import APIError from '../helpers/APIError';
+import pages from "../controllers/pages";
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
@@ -11,59 +12,79 @@ const ObjectId = Schema.ObjectId;
  */
 const Pages = new mongoose.Schema({
 
-    slug: {
-        type: String,
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: Object,
+  slug: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: Object,
 
-    },
+  },
 
 });
 
 Pages.statics = {
 
-    get(pageURL) {
-        return this.findOne({ slug: pageURL, $or: [{ state: 'published' }, { state: 'draft' }] }).execAsync()
-            .then((page) => {
-            if (page) {
-                return page;
-            }
+  get (pageURL) {
+    return this.findOne({slug: pageURL, $or: [{state: 'published'}, {state: 'draft'}]}).execAsync()
+      .then((page) => {
+        if (page) {
+          return page;
+        }
 
-            const err = new APIError('No such page exists!----', httpStatus.NOT_FOUND, true);
-            return Promise.reject(err);
-        })
-          .catch((e) => {
-            console.log(e);
-            const err = new APIError('No such page exists!', httpStatus.NOT_FOUND, true);
-            return Promise.reject(err);
-        });
-    },
+        const err = new APIError('No such page exists!----', httpStatus.NOT_FOUND, true);
+        return Promise.reject(err);
+      })
+      .catch((e) => {
+        console.log(e);
+        const err = new APIError('No such page exists!', httpStatus.NOT_FOUND, true);
+        return Promise.reject(err);
+      });
+  },
 
-    getPagesList() {
-        return this.find().select({ title: 1, slug: 1, state: 1, putOnFooter: 1, externalURL:1 })
-          .then((pages) => {
-            if (!pages) {
-              const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
-              return Promise.reject(err);
-            }
-            return pages;
-
-        })
-          .catch((e)=> {
-            const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
-            return Promise.reject(err);
-        });
-    },
+  getPagesList() {
+    return this.aggregate([
+      {$group: {_id: "$category", values: {$push: "$$ROOT"}}},
+      // {
+      //   "$project": {
+      //     title: 1, slug: 1, state: 1, putOnFooter: 1, externalURL: 1, category: 1
+      //   }
+      // }
+    ]).then(pages => {
+      if (!pages) {
+        const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
+        return Promise.reject(err);
+      }
+      return pages;
+    })
+      .catch((e) => {
+        const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
+        return Promise.reject(err);
+      });
+  },
+  // getPagesList() {
+  //   return this.find().select({title: 1, slug: 1, state: 1, putOnFooter: 1, externalURL: 1, category: 1}).lean()
+  //     .then((pages) => {
+  //       if (!pages) {
+  //         const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
+  //         return Promise.reject(err);
+  //       }
+  //       return pages;
+  //
+  //     })
+  //     .catch((e) => {
+  //       const err = new APIError('Error while requesting pages list!----', httpStatus.NOT_FOUND, true);
+  //       return Promise.reject(err);
+  //     });
+  // },
 
 };
 
